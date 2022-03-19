a0241d58ba8f37aa44b34a41b51a153486c307f3
Victor Santoscoy <victor.santoscoy1731@gmail.com>
2022-03-19 17:14:23 -0600

    adding modified playRound to script.js

--- script.js
+++ script.js
@@ -3,18 +3,35 @@ console.log("Hola, Victor");
 
 let computerPlay = () => {
     let options = ["paper","rock","scisors"]
-    // let x = Math.round(Math.random()* 2)
-    let x = 1;
+    let x = Math.round(Math.random()* 2)
+    // let x = 1;
     result = options[x];
     return result;
 }
 
 let playRound = (playerSelection,computerSelection) => {  
+    let result;
     let strPlayerSelection = playerSelection.toLowerCase(); 
-    console.log(`Computer chose: ${computerSelection}\nUser chose: ${strPlayerSelection}\n`);
-    if (strPlayerSelection === computerSelection){
-        return console.log("It's a Draw");
+    if (strPlayerSelection === "paper" || strPlayerSelection === "rock" || strPlayerSelection === "scisors"){
+        console.log(`Computer chose: ${computerSelection}\nUser chose: ${strPlayerSelection}\n`);
+        if (strPlayerSelection === computerSelection){
+            return "It's a Draw";
+        }
+        else if(computerSelection === "paper"){
+            result = strPlayerSelection === "scisors" ? "User win!!" : "Computer win!!";
+            return result;
+        }
+        else if(computerSelection === "rock"){
+            result = strPlayerSelection === "scisors" ? "Computer win!!" : "User win!!";
+            return result;
+        }
+        else if(computerSelection === "scisors"){
+            result = strPlayerSelection === "paper" ? "Computer win!!" : "User win!!";
+            return result;
+        }
     }
-    
+    else {
+        return "invalid input";
+    }
 }
-playRound("rOcK",computerPlay());
+console.log(playRound("PAPER",computerPlay()));
