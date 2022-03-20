cf64a9f3dae9ff1df190bf7a719cbd2fcff9c67c
Victor Santoscoy <victor.santoscoy1731@gmail.com>
2022-03-19 18:01:04 -0600

    adding game() function to script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("Hola, Victor");
 
-
 let computerPlay = () => {
     let options = ["paper","rock","scisors"]
     let x = Math.round(Math.random()* 2)
     // let x = 1;
     result = options[x];
     return result;
+
 }
 
 let playRound = (playerSelection,computerSelection) => {  
@@ -15,7 +15,7 @@ let playRound = (playerSelection,computerSelection) => {
     if (strPlayerSelection === "paper" || strPlayerSelection === "rock" || strPlayerSelection === "scisors"){
         console.log(`Computer chose: ${computerSelection}\nUser chose: ${strPlayerSelection}\n`);
         if (strPlayerSelection === computerSelection){
-            return "It's a Draw";
+            return "It's a draw";
         }
         else if(computerSelection === "paper"){
             result = strPlayerSelection === "scisors" ? "User win!!" : "Computer win!!";
@@ -34,4 +34,44 @@ let playRound = (playerSelection,computerSelection) => {
         return "invalid input";
     }
 }
-console.log(playRound("PAPER",computerPlay()));
+
+let game = () => {
+    let computerWins = 0;
+    let userWins = 0;
+    let draws = 0;
+    for (let i = 0 ; i < 5; i++){
+        let userChoise = prompt("Paper, rock or scisors","PAPER");
+        let computerChoise = computerPlay();
+        let result = playRound(userChoise,computerChoise);
+        if (result === "It's a draw")
+        {
+            draws += 1;
+        }
+        else if( result === "Computer win!!")
+        {
+            computerWins += 1;
+        }
+        else
+        {
+            userWins += 1;
+        }
+        console.log(result + `\nComputer points: ${computerWins}\nUser wins: ${userWins}\nDraws: ${draws}`);
+    }
+    if(computerWins === userWins)
+    {
+        return "It was a good battle but was a draw :(";
+    }
+    else if(computerWins > userWins)
+    {
+        return "COMPUTER IS THE FINAL WINEEEER!!";
+    }
+    else
+    {
+        return "USER IS THE FINAL WINER!!";
+    }
+}
+console.log(game());
+
+
+
+// console.log(playRound("PAPER",computerPlay()));
